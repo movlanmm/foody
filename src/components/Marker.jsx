7d84576001098db515a359d81e9965ad98da8635
Movlan <movlan.mustafayev@gmail.com>
2025-01-07 13:35:06 +0400

    marker pin

--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -4,13 +4,11 @@ import "leaflet/dist/leaflet.css";
 import { Marker, useMap } from "react-leaflet";
 import { FaMapMarkerAlt } from "react-icons/fa";
 
-
-
 const CustomMarker = ({ position, children }) => {
   const map = useMap();
 
   const customIcon = L.icon({
-    iconUrl: '/public/images/pin.png',
+    iconUrl: "/images/pin.png",
     iconSize: [25, 40],
     iconAnchor: [25, 50],
   });
